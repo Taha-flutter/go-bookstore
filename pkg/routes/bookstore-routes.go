cf31e491dd6c880aa6c1805cd57b386f4dcf8d9f
taha khan <Taha@TAF1158.com>
2025-03-18 19:56:53 +0500

    feat: add PrintMiddleware and update bookstore routes for consistency

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -2,13 +2,15 @@ package routes
 
 import (
 	"github.com/Taha-flutter/go-bookstore/pkg/controllers"
+	"github.com/Taha-flutter/go-bookstore/pkg/middleware"
 	"github.com/gorilla/mux"
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.Use(middleware.PrintMiddleware)
 	router.HandleFunc("/books", controllers.GetBooks).Methods("GET")
-	router.HandleFunc("/books/{id}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("/books", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/books", controllers.UpdateBook).Methods("PUT")
-	router.HandleFunc("/books/{id}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/{id}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/{id}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{id}", controllers.DeleteBook).Methods("DELETE")
 }
